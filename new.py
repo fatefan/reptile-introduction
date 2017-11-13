1e9a904cfb1540598c860167b273428e80affa0c
fynn <fatefan@users.noreply.github.com>
2017-11-13 15:43:48 +0800

    输入本地文件
    将获取的数据写入本地文件

--- new.py
+++ new.py
@@ -7,3 +7,6 @@ source = requests.post('http://www.ftchinese.com/rss/news') #获取FT中文 RSS 
 root = ET.fromstring(source.text.encode('utf-8')) # 解析 XML
 for title in root.iter('title'):
     print title.text.encode('utf-8') # 打印title
+fo = open('news.xml','w') # 打开一个文件
+fo.write(source.text.encode('utf-8')) # 写入文件
+fo.close() # 关闭文件
